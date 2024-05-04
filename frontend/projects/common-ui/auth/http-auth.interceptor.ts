ea646da3675ccaad29fc600ca60b3d0de4c43baf
Tome Ivanov <tome_ivanov@windowslive.com>
2024-05-05 00:37:29 +0200

    fix injector bug

--- frontend/projects/common-ui/auth/http-auth.interceptor.ts
+++ frontend/projects/common-ui/auth/http-auth.interceptor.ts
@@ -1,9 +1,9 @@
-import { Injectable, inject } from '@angular/core';
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHandlerFn, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, switchMap } from "rxjs/operators";
 import { AuthService } from '../auth/auth.service';
-import { AuthSignal, updateAuth } from './auth.signal';
+import { AuthSignal, logOut, updateAuth } from './auth.signal';
 
 export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
     if (request.url.includes('auth/refresh')) {
@@ -17,32 +17,34 @@ export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, ne
       request = injectToken(request);
     }
 
+    const authService = inject(AuthService);
+
     return next(request).pipe(catchError(error => {
       if (error instanceof HttpErrorResponse && error.status === 401) {
-        return handle401Error(request, next);
+        const refreshToken = AuthSignal().refreshToken;
+
+        if (!refreshToken) {
+          return next(request);
+        }
+        
+        return authService.refresh().pipe(
+          switchMap(data => {
+            const token = data.token;
+            const oldVal = AuthSignal();
+            oldVal.token = token;
+            updateAuth(oldVal);
+            return next(injectToken(request));
+          }),
+          catchError(error => {
+            if (error instanceof HttpErrorResponse && error.status === 401) {
+              logOut();
+            }
+            return throwError(() => error);
+          }),
+        );
       }
       return throwError(() => error);
     }));
-  }
-
-const handle401Error = (request: HttpRequest<any>, next: HttpHandlerFn) => {
-    const refreshToken = AuthSignal().refreshToken;
-
-    if (!refreshToken) {
-      return next(request);
-    }
-
-    const authService = inject(AuthService);
-
-    return authService.refresh().pipe(
-      switchMap(data => {
-        const token = data.token;
-        const oldVal = AuthSignal();
-        oldVal.token = token;
-        updateAuth(oldVal);
-        return next(injectToken(request));
-      })
-    );
   }
 
 const injectToken = (request: HttpRequest<any>): HttpRequest<any> => {
