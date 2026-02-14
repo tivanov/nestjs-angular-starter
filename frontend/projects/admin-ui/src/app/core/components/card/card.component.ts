aae84fa341620ca43e8803742ebffecda1e9bf6e
borisdaev <borce-daev@hotmail.com>
2026-02-14 20:15:36 +0100

    collapsible-card

--- frontend/projects/admin-ui/src/app/core/components/card/card.component.ts
+++ frontend/projects/admin-ui/src/app/core/components/card/card.component.ts
@@ -1,14 +1,28 @@
-
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  signal,
+} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { BaseComponent } from '../../../../../../common-ui/base/base.component';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent extends BaseComponent {
   public readonly titleText = input<string>();
+  public readonly collapsible = input<boolean>(true);
+  protected readonly collapsed = signal(false);
+
+  protected toggleCollapsed() {
+    if (this.collapsible()) {
+      this.collapsed.update((v) => !v);
+    }
+  }
 }
